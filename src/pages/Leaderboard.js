efdb7101065f2ab5309c6363c560c126eaccb910
ipheiman <54426351+ipheiman@users.noreply.github.com>
2020-09-22 03:28:56 +0800

    added leaderboard page

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,14 +1,44 @@
 import React from 'react'
-
+import './Leaderboard.css'
 function Leaderboard() {
-    document.body.style = 'background:  #ECECEC'; 
+    document.body.style = 'background:  #ECECEC';
     return (
-        <div className="leaderboard">
+        <div className="leaderboard-page">
             <header>
-            <h1>Welcome to Leaderboard</h1>
+                <h1>Welcome to Leaderboard</h1>
 
             </header>
-        </div>
+            <div class="leaderboard">
+                <h1>
+                    <strong>
+                    BEST EMPLOYEE OF SEPTEMBER 2020
+                    </strong>
+                </h1>
+                <ol>
+                    <li>
+                        <mark>Heiman</mark>
+                        <small>315</small>
+                    </li>
+                    <li>
+                        <mark>Heiman</mark>
+                        <small>301</small>
+                    </li>
+                    <li>
+                        <mark>Heiman</mark>
+                        <small>292</small>
+                    </li>
+                    <li>
+                        <mark>Heiman</mark>
+                        <small>245</small>
+                    </li>
+                    <li>
+                        <mark>Heiman</mark>
+                        <small>203</small>
+                    </li>
+                </ol>
+            </div>
+
+        </div >
     )
 }
 
