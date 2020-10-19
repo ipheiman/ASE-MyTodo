6617c586b8b49329179530b71ab66dcc6ad488ca
ipheiman <54426351+ipheiman@users.noreply.github.com>
2020-10-19 19:51:54 +0800

    "each_sideproject" needs to have its own "eachtask"

--- src/components/TaskTesting/Each_sideproject.js
+++ src/components/TaskTesting/Each_sideproject.js
@@ -6,8 +6,8 @@ function Each_sideproject(props) {
         props.setSideProjects(props.sideProjects.filter(el => el.projectId !== props.sideProject.projectId))
     }
     return (
-        <div>
-            <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="/timer" role="tab" aria-controls="v-pills-home" aria-selected="true">{props.text}
+        <div className = "side_project">
+            <a className="nav-link" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">{props.text}
             <button onClick={deleteHandler} className="trash-btn-pill text-danger"><i className="fas fa-trash"></i></button>
             </a>
         </div>
--- src/components/TaskTesting/Task_list.js
+++ src/components/TaskTesting/Task_list.js
@@ -9,56 +9,58 @@ function Task_list(props) {
             {/* SIDEBAR */}
             <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 <div>
-                {props.sideProjects.map((sideProject) => (
-                            <Each_sideproject
-                                text={sideProject.sideProjectText}
-                                key={sideProject.projectId}
-                                sideProject={sideProject}
-                                setSideProjects={props.setSideProjects}
-                                sideProjects={props.sideProjects}
-                            />
-                        ))}
+                    {props.sideProjects.map((sideProject) => (
+                        <Each_sideproject
+                            text={sideProject.sideProjectText}
+                            key={sideProject.projectId}
+                            sideProject={sideProject}
+                            setSideProjects={props.setSideProjects}
+                            sideProjects={props.sideProjects}
+
+                            // pass the states and hooks for
+                        />
+                    ))}
 
                 </div>
-
-                {/* <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Projects<i className="pillIcon"><AiIcons.AiOutlinePlus /></i></a>
-                <a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Projects<i className="pillIcon"><AiIcons.AiOutlinePlus /></i></a> */}
-
             </div>
-            {/* TABLE */}
+            {/* SIDEBAR CONTENT */}
+            <div class="tab-content" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
 
-            <div className="task-table container">
-                {/* <table className="table table-hover" id="project-table"> */}
-                <table className="task-table">
-                    <thead className="text-center">
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Due Date</th>
-                            <th scope="col">Priority</th>
-                            <th scope="col">Reminder Date</th>
+                    <div className="task-table container">
+                        {/* <table className="table table-hover" id="project-table"> */}
+                        <table className="task-table">
+                            <thead className="text-center">
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Due Date</th>
+                                    <th scope="col">Priority</th>
+                                    <th scope="col">Reminder Date</th>
 
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {props.filteredTasks.map((task) => (
-                            <Each_task
-                                text={task.text}
-                                description={task.description}
-                                priority={task.priority}
-                                date={task.date}
-                                reminderDate={task.reminderDate} 
-                                key={task.id}
-                                task={task}
-                                setTasks={props.setTasks}
-                                tasks={props.tasks}
-                            />
-                        ))}
-                    </tbody>
-                </table>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.filteredTasks.map((task) => (
+                                    <Each_task
+                                        text={task.text}
+                                        description={task.description}
+                                        priority={task.priority}
+                                        date={task.date}
+                                        reminderDate={task.reminderDate}
+                                        key={task.id}
+                                        task={task}
+                                        setTasks={props.setTasks}
+                                        tasks={props.tasks}
+                                    />
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
             </div>
-
         </div>
     )
 }
