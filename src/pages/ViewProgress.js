e07ac14d325786014b630c95e83001735d985060
reenaxxraj <44928185+reenaxxraj@users.noreply.github.com>
2020-10-21 02:54:20 +0800

    Show change in project

--- src/pages/ViewProgress.js
+++ src/pages/ViewProgress.js
@@ -1,53 +1,132 @@
-import React from 'react'
+import React, {useState, setState} from 'react'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-import {
-    dailySalesChart,
+import {dailySalesChart,
     emailsSubscriptionChart,
-    completedTasksChart
-} from "../components/DataVisualisation/chart"
+    completedTasksChart} from "../components/DataVisualisation/chart"
+
+    import {Line} from 'react-chartjs-2'
+    
+import {Bar} from 'react-chartjs-2'
 
 import LineChart from "../components/DataVisualisation/LineChart"
 import BarChart from "../components/DataVisualisation/BarChart"
 
 function ViewProgress() {
-    const [state, setState] = React.useState({
-        ProjectId: ""
-    })
+
+    const [ProjectId, setProjectId] = useState(0);
+
+    const data1 = { 
+        labels: [
+            "Sunday",
+            "Monday",
+           "Tuesday",
+           "Wednesday",
+           "Thursday",
+           "Friday",
+           "Saturday"
+        ],
+
+        datasets: [
+            {
+                label: "Number of tasks completed",
+                data: [3 , 3, 0, 4,5, 7,8] ,
+                borderColor : ['#17A2B8'],
+                backgroundColor : ['#17A2B8'],
+                pointBackgroundColor : ['#e3f2fd'],
+                pointBorderColor : ['rgba(54,162,235,0,2)']
+            
+            }
+        ]
+        }
+
+        const data2 = { 
+            labels: [
+                "Sunday",
+                "Monday",
+               "Tuesday",
+               "Wednesday",
+               "Thursday",
+               "Friday",
+               "Saturday"
+            ],
+    
+            datasets: [
+                {
+                    label: "Number of tasks completed",
+                    data: [0 , 3, 0, 3,3, 2,8] ,
+                    borderColor : ['#17A2B8'],
+                    backgroundColor : ['#17A2B8'],
+                    pointBackgroundColor : ['#e3f2fd'],
+                    pointBorderColor : ['rgba(54,162,235,0,2)']
+                
+                }
+            ]
+            }
+
+            const data3 = { 
+                labels: [
+                    "Sunday",
+                    "Monday",
+                   "Tuesday",
+                   "Wednesday",
+                   "Thursday",
+                   "Friday",
+                   "Saturday"
+                ],
+        
+                datasets: [
+                    {
+                        label: "Number of tasks completed",
+                        data: [4 , 9, 1, 4,5, 7,5] ,
+                        borderColor : ['#17A2B8'],
+                        backgroundColor : ['#17A2B8'],
+                        pointBackgroundColor : ['#e3f2fd'],
+                        pointBorderColor : ['rgba(54,162,235,0,2)']
+                    
+                    }
+                ]
+                }
+    const data = [data1,data2,data3]
+
     const options = [
         'Project1', 'Project2', 'Project3'
-    ];
-    const defaultOption = options[0];
-
+      ];
+    
+    
     function handleChange(event) {
-        console.log("Project changed");
-        setState({
-            //[name] : event.target.value
-        })
+        console.log(ProjectId)
+        console.log(data[ProjectId])
+        setProjectId(event.target.value)
     }
-
+      
     return (
         <div className="viewprogress">
             <header>
-                <h1>
-                    Progress
+            <h1>
+                View Progress
             </h1>
             </header>
             <body>
-                <div style={{ "width": "200px", "margin-left": "80%", "margin-bottom": "50px" }}>
-                    <div className="dropdown select">
-                        <select className="filter-todo browser-default custom-select">
-                            <option value="Project 1">Project 1</option>
-                            <option value="Project 2">Project 2</option>
-                            <option value="Project 3">Project 3</option>
-                        </select>
-                    </div>
-                    {/* <Dropdown size = {1} options={options} onChange={handleChange} value={defaultOption} placeholder="Select an option" /> */}
+            <div  style={{"width": "200px", "margin-left":"80%", "margin-bottom":"50px"}}>
+            <div className="dropdown select">
+                    <select  className="filter-todo browser-default custom-select" onChange = {handleChange}>
+                        <option value = {0}>Project 1</option>
+                        <option value = {1}>Project 2</option>
+                        <option value = {2}>Project 3</option>
+                    </select>
                 </div>
+            </div>
 
-                <LineChart />
+            {/* <LineChart data = {data1} /> */}
 
-                <BarChart />
+            <div style={{"height" : "50%", "width": "50%", "display": "inline-block"}}>
+            <Line data = {data[ProjectId]}/>
+            </div>
+
+            <div style={{"height" : "50%", "width": "50%","display": "inline-block"}}>
+        <Bar data = {data[ProjectId]}/>
+        </div>
 
             </body>
         </div>
